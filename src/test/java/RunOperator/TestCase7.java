1547f318426be35a0242d2e51b68e3d46f7521d9
Enes <enes.buke12@gmail.com>
2025-01-27 21:32:55 +0300

    Last 4 tests and allTestCases updated

---
+++ src/test/java/RunOperator/TestCase7.java
@@ -0,0 +1,22 @@
+package RunOperator;
+
+import Elements.PrElement;
+import Utility.BaseDriver;
+import org.testng.Assert;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class TestCase7 extends BaseDriver {
+    @Test
+    @Parameters("msg")
+    public void US_07_Search(String text){
+
+        PrElement pr=new PrElement();
+        pr.searchStore.sendKeys(text);
+        pr.searchButton.click();
+        pr.product.click();
+
+        Assert.assertTrue(pr.productName.getText().contains("Adobe Photoshop CS4"));
+
+    }
+}
